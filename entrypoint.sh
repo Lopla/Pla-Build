220a03fd8d821d9b20c14e6651383e52849a536e
Paweł Gałęzowski <pgalezowski@gmail.com>
2021-01-03 22:20:04 +0100

    fixed some paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,8 +11,16 @@ cd Pla.Mobile.Android
 
 echo "Compiling project"
 msbuild /p:Configuration=Release /t:PackageForAndroid  Pla.Mobile.Android.csproj
+
+
+echo "Extracting keystore data"
 echo $1 | base64 --decode > keystore
-        
+
+echo "Signing the package"
 jarsigner -verbose -sigalg MD5withRSA -digestalg SHA1 -keystore keystore -storepass $2 -signedjar ~/pla_signed.apk bin/Release/com.companyname.pla.mobile.apk Pla
+
+echo "Creating properrly zipaligned package"
 ~/Library/Android/sdk/build-tools/30.0.1/zipalign -f -v 4 ~/pla_signed.apk ~/pla.apk
-        
+
+echo "Done"
+ls -l ~/pla.apk
