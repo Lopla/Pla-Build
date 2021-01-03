378fa1d48cdf13c33f49cc03daaadd4a85a10a74
Paweł Gałęzowski <pgalezowski@gmail.com>
2021-01-03 22:18:35 +0100

    some script fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,18 @@
 #!/bin/sh
 
-echo "Starting here"
-pwd 
-ls 
-
 echo "Picking up the SDK from root folder"
-mv /Pla-refs-heads-master_2021-01-04-02-44-07 ./Pla-SDK
-ls
+mv /Pla-refs-heads-master_2021-01-04-02-44-07/Pla.Mobile ./Pla.Mobile
 
-echo "I will not build it yet..."
 
-# cd Pla
+cd Pla.Mobile
+echo "Restoring solution nugets"
+nuget restore Pla.Mobile.sln
+cd Pla.Mobile.Android
 
-# cd Pla.Mobile
-# nuget restore Pla.Mobile.sln
-# cd Pla.Mobile.Android
-
-# msbuild /p:Configuration=Release /t:PackageForAndroid  Pla.Mobile.Android.csproj
-# echo $KEYSTORE | base64 --decode > keystore
+echo "Compiling project"
+msbuild /p:Configuration=Release /t:PackageForAndroid  Pla.Mobile.Android.csproj
+echo $1 | base64 --decode > keystore
         
-# jarsigner -verbose -sigalg MD5withRSA -digestalg SHA1 -keystore keystore -storepass $KEYSTORE_PASSWORD -signedjar ~/pla_signed.apk bin/Release/com.companyname.pla.mobile.apk Pla
-# ~/Library/Android/sdk/build-tools/30.0.1/zipalign -f -v 4 ~/pla_signed.apk ~/pla.apk
+jarsigner -verbose -sigalg MD5withRSA -digestalg SHA1 -keystore keystore -storepass $2 -signedjar ~/pla_signed.apk bin/Release/com.companyname.pla.mobile.apk Pla
+~/Library/Android/sdk/build-tools/30.0.1/zipalign -f -v 4 ~/pla_signed.apk ~/pla.apk
         
