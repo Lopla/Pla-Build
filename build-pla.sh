7722d3caba6210a85549b7c436347fa1b051bac5
Paweł Gałęzowski <pgalezowski@gmail.com>
2021-01-03 21:17:56 +0100

    fic

---
+++ build-pla.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd Pla
+
+cd Pla.Mobile
+nuget restore Pla.Mobile.sln
+cd Pla.Mobile.Android
+
+msbuild /p:Configuration=Release /t:PackageForAndroid  Pla.Mobile.Android.csproj
+echo $KEYSTORE | base64 --decode > keystore
+        
+jarsigner -verbose -sigalg MD5withRSA -digestalg SHA1 -keystore keystore -storepass $KEYSTORE_PASSWORD -signedjar ~/pla_signed.apk bin/Release/com.companyname.pla.mobile.apk Pla
+~/Library/Android/sdk/build-tools/30.0.1/zipalign -f -v 4 ~/pla_signed.apk ~/pla.apk
+        
+ls ~/
